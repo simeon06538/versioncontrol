0a319d2219e9966d571fa4efb38911e42f80db51
simeon06538 <simeoncjorji@gmail.com>
2025-09-20 04:03:22 -0500

    first steps

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print("yo")
-print("hello")
+import serial
+
+ser = serial.Serial('COM3', 9600, timeout = 1)
+
+print("scan barcode. press Ctrl+C to exit.")
+try:
+  while True:
+    barcode = ser.readline().decode('utf-8').strip()
+    if barcode:
+      print("Scanned barcode")
+except KeyboardInterrupt:
+  print("Do not touch keyboard")
+  ser.close()
